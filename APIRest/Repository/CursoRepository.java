e61c7d8ebee1a338db77d679c97013f8aa0ddafa
VieraxD <123433729+VieraxD8@users.noreply.github.com>
2024-07-13 19:00:07 -0500

    Delete APIRest directory
    borrar

--- APIRest/Repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.APIRest.APIRest.Repository;
-
-import com.APIRest.APIRest.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-}
--- APIRest/Repository/PerfilRepository.java
+++
@@ -1,7 +0,0 @@
-package com.APIRest.APIRest.Repository;
-
-import com.APIRest.APIRest.Perfil;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerfilRepository extends JpaRepository<Perfil, Long> {
-}
